8156adbd9c083635bb96cd573e5d0d5811eb3367
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-24 14:02:46 +0200

    Add files via upload

---
+++ JavaBook/Example.java
@@ -0,0 +1,12 @@
+/*
+Простая программа на Java .
+Назовите этот файл Example . j ava .
+*/
+
+
+public class Example {
+    // Программа начинается с вызова main () .
+    public static void main(String[] args) {
+        System.out.println("Simple program in Java languages");
+    }
+}
