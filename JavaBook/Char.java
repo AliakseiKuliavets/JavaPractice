1d054b11a290a73c4798589dd0e5ab587405b0ac
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-06-30 12:18:30 +0200

    Add files via upload

---
+++ JavaBook/Char.java
@@ -0,0 +1,11 @@
+public class Char {
+    public static void main(String[] args)
+            throws java.io.IOException{ // конструкция которая необходима для обработки ошибок ввода
+        char choice;
+        choice = (char) System.in.read();
+        // который читает
+        //символы из стандартного ввода (возвращаемые в виде целых чисел, по
+        //причине чего возвращаемое значение было преобразовано в char).
+        System.out.println(choice);
+    }
+}
---
+++ JavaBook/Sample.java
@@ -0,0 +1,9 @@
+public class Sample {
+    public static void main(String[] args) {
+        int a,b;
+        for ( a = 1, b = 10; a < b ; a ++ , b--) {
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+        }
+    }
+}
